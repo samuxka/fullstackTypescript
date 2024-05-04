246651b5b908d1f56ad16a7cd928338dd01fde81
Samuel <109113326+samuxka@users.noreply.github.com>
2024-05-04 00:35:24 -0300

    fundamentos

---
+++ typescript/fundamentos/aula01/app.ts
@@ -0,0 +1,6 @@
+const soma = (a: number, b: number) => {
+    console.log(a + b)
+}
+
+soma(1, 2)
+//soma('Samuel ', 'Oliveira') | Aqui vai dar erro!
---
+++ typescript/fundamentos/aula01/index.js
@@ -0,0 +1,6 @@
+const soma = (a, b) => {
+    console.log(a + b)
+}
+
+soma(8,9)
+soma("Samuel ", "Oliveira")
